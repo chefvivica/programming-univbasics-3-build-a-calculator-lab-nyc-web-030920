e64e8051cf703ca40b2c4f6a2b7a6049e3be67f9
Vivica Ma <vivicacongma@gmail.com>
2020-02-10 16:57:00 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,12 +9,13 @@ sum
 end
 
 def division(num1, num2)
-puts  = num1 + num2
+puts  product = num1 * num2
 sum 
 end
 
 def multiplication(num1, num2)
-
+puts  product = num1 * num2
+sum 
 end
 
 def modulo(num1, num2)
